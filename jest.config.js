59b42ab36d639b4f47ce5fa2c71c991e1bb7a0f7
Ahmed Hilali <monstrobishi@gmail.com>
2021-06-03 20:14:36 +0800

    Added initial packages and config files (#17)
    * Added packages and config files
    
    * Updated packages
    
    * Tests passing
    
    * Moved price functions
    
    * Added token expression
    
    * Attempt to fi x linter
    
    * Removed tokens from tests
    
    * Removed tsbuildinfo
    
    * Added build info to gitignore
    
    * Removed tsbuildconfig
    
    * Push build info
    
    * Delete build info
    
    * Add ignore build info

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: '((\\.|/)(e2e|test|spec))\\.[jt]sx?$',
   moduleNameMapper: {
-    '@defichain/salmon-oracles-functions': '<rootDir>/packages/salmon-oracles-functions/src'
+    '@defichain/salmon-oracles-functions': '<rootDir>/packages/salmon-oracles-functions/src',
+    '@defichain/salmon-price-functions': '<rootDir>/packages/salmon-price-functions/src',
+    '@defichain/salmon-lambda-functions': '<rootDir>/packages/salmon-lambda-functions/src'
   },
   testTimeout: 240000
 }
