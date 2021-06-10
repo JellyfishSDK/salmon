c51c32b38bb2957ff7c0a8e7795b9f4297b403c6
Ahmed Hilali <monstrobishi@gmail.com>
2021-06-10 22:55:53 +0800

    Removed unused testutils

--- packages/salmon-oracles-functions/__tests__/test.utils.ts
+++ packages/salmon-oracles-functions/__tests__/test.utils.ts
@@ -2,39 +2,12 @@ import { Bech32, Elliptic, EllipticPair, dSHA256 } from '@defichain/jellyfish-cr
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { CTransaction, CTransactionSegWit, Transaction, TransactionSegWit } from '@defichain/jellyfish-transaction'
 import { SmartBuffer } from 'smart-buffer'
-import { BigNumber } from 'bignumber.js'
 
 /**
  * For test mocking only, obviously not secured.
  */
 export function randomEllipticPair (): EllipticPair {
   return Elliptic.fromPrivKey(Buffer.alloc(32, Math.random().toString(), 'ascii'))
-}
-
-/**
- * Clear all balance
- */
-export async function clearEllipticPairBalance (container: MasterNodeRegTestContainer, ellipticPair: EllipticPair): Promise<void> {
-  const pubKey = await ellipticPair.publicKey()
-  const address = Bech32.fromPubKey(pubKey, 'bcrt')
-
-  const unspent: any[] = await container.call('listunspent', [1, 9999999, [address]])
-  if (unspent.length === 0) {
-    return
-  }
-
-  const reuse = await container.getNewAddress()
-  const amount: BigNumber = unspent
-    .reduce((prev, utxo) => {
-      return new BigNumber(utxo.amount).plus(prev)
-    }, new BigNumber(0))
-  const hex = await container.call('createrawtransaction', [
-    unspent.map(({ txid, vout }) => ({ txid, vout })), [{ [reuse]: amount.minus(0.00005).toNumber() }]
-  ])
-
-  const signed = await container.call('signrawtransactionwithwallet', [hex])
-  await container.call('sendrawtransaction', [signed.hex])
-  await container.generate(1)
 }
 
 /**
@@ -75,17 +48,4 @@ export interface TxOut {
     addresses: string[]
   }
   tokenId: number
-}
-
-export async function findOuts (outs: TxOut[], pair: EllipticPair): Promise<TxOut[]> {
-  const pubKey = await pair.publicKey()
-  const address = Bech32.fromPubKey(pubKey, 'bcrt')
-
-  return outs.filter(value => {
-    return value.scriptPubKey.addresses?.includes(address)
-  })
-}
-
-export async function findOut (outs: TxOut[], pair: EllipticPair): Promise<TxOut> {
-  return (await findOuts(outs, pair))[0]
 }
