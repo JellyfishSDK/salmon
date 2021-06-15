1d1672516104a36385af7772b7aa2b36379c5c24
Ahmed Hilali <monstrobishi@gmail.com>
2021-06-15 22:04:11 +0800

    Updated build config

--- packages/salmon-lambda-functions/src/index.ts
+++ packages/salmon-lambda-functions/src/index.ts
@@ -1,15 +1,22 @@
 import { PriceManager, PriceSourceConfig, TiingoPriceProvider } from '@defichain/salmon-price-functions'
 
-export async function handler (event: any): Promise<any> {
+export async function handler (event?: any): Promise<any> {
   const tiingoConfig: PriceSourceConfig = {
     symbols: ['TSLA']
   }
 
-  const priceManager = new PriceManager(tiingoConfig, new TiingoPriceProvider('API_TOKEN'))
+  const priceManager = new PriceManager(tiingoConfig, new TiingoPriceProvider(process.env.API_TOKEN ?? ''))
   const prices = await priceManager.fetchAssetPrices()
+
+  console.log(JSON.stringify(prices))
 
   return {
     statusCode: 200,
     body: JSON.stringify({ prices, event })
   }
 };
+
+handler().then(
+  () => {},
+  () => {}
+)
