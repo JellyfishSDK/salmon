77cad3cc7deaa1e6ca761caf7e438bf72c6c7252
Ahmed Hilali <monstrobishi@gmail.com>
2021-06-15 20:24:55 +0800

    Basic lambda function

---
+++ packages/salmon-lambda-functions/src/index.ts
@@ -0,0 +1,17 @@
+import { AssetPrice, FinnhubbPriceProvider, IexPriceProvider, PriceManager, PriceSourceConfig, TiingoPriceProvider } from '../src'
+import BigNumber from 'bignumber.js'
+
+export async function handler (event)  {
+  const tiingoConfig: PriceSourceConfig = {
+    symbols: ['TSLA']
+  }
+
+  const priceManager = new PriceManager(tiingoConfig, new TiingoPriceProvider('API_TOKEN'))
+  const prices = await priceManager.fetchAssetPrices()
+
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify(prices),
+  };
+  return response;
+};
