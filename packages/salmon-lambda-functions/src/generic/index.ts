2712f27b53ce4540903d50fb96ee31acd0f3272e
Ahmed Hilali <monstrobishi@gmail.com>
2021-06-24 17:29:31 +0800

    Added timestamp filter to lambda functions (#58)
    * Added timestamp filter to lambda functions
    
    * Combined redundent code
    
    * Combined redundant code
    
    * split generic handler

---
+++ packages/salmon-lambda-functions/src/generic/index.ts
@@ -0,0 +1,52 @@
+import { PriceManager, PriceProvider, AssetPrice } from '@defichain/salmon-price-functions'
+import { OraclesManager } from '@defichain/salmon-oracles-functions'
+
+interface EnvironmentConfig {
+  oceanUrl: string
+  network: string
+  oracleId: string
+  currency: string
+  symbols: string[]
+  intervalSeconds: number
+  privateKey: string
+}
+
+const getEnvironmentConfig = (): EnvironmentConfig => {
+  return {
+    oceanUrl: process.env.OCEAN_URL ?? 'localhost',
+    network: process.env.NETWORK ?? 'regtest',
+    oracleId: process.env.ORACLE_ID ?? '',
+    currency: process.env.CURRENCY ?? 'USD',
+    symbols: (process.env.SYMBOLS ?? '').split(','),
+    intervalSeconds: parseInt(process.env.INTERVAL_SECONDS ?? '300'),
+    privateKey: process.env.PRIVATE_KEY ?? '' //! TODO: Fetch some other way perhaps
+  }
+}
+
+const broadcastPrices = async (env: EnvironmentConfig, prices: AssetPrice[]): Promise<void> => {
+  const oraclesManager = OraclesManager.withWhaleClient(env.oceanUrl, env.network, env.privateKey)
+  await oraclesManager.updatePrices(env.oracleId,
+    prices.map(assetPrice => ({
+      token: assetPrice.asset,
+      prices: [{ currency: env.currency, amount: assetPrice.price }]
+    })))
+}
+
+const fetchPrices = async (env: EnvironmentConfig, provider: PriceProvider): Promise<AssetPrice[]> => {
+  const priceManager = new PriceManager({ symbols: env.symbols }, provider)
+  return PriceManager.filterTimestamps(await priceManager.fetchAssetPrices(),
+    new Date(env.intervalSeconds * 1000.0))
+}
+
+export async function handleGenericPriceApiProvider (provider: PriceProvider, event?: any): Promise<any> {
+  const env = getEnvironmentConfig()
+  const prices = await fetchPrices(env, provider)
+  await broadcastPrices(env, prices)
+
+  console.log(JSON.stringify({ prices, event }))
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ prices, event })
+  }
+}
--- packages/salmon-lambda-functions/src/iexcloud.ts
+++ packages/salmon-lambda-functions/src/iexcloud.ts
@@ -1,36 +1,7 @@
-import { PriceManager, PriceSourceConfig } from '@defichain/salmon-price-functions'
+import { handleGenericPriceApiProvider } from './generic'
 import { IexPriceProvider } from '@defichain/salmon-provider-iexcloud'
-import { OraclesManager } from '@defichain/salmon-oracles-functions'
 
 export async function handler (event?: any): Promise<any> {
-  // Fetch env vars at the top here so it's clearer
   const apiToken = process.env.API_TOKEN ?? ''
-  const oceanUrl = process.env.OCEAN_URL ?? 'localhost'
-  const network = process.env.NETWORK ?? 'regtest'
-  const oracleId = process.env.ORACLE_ID ?? ''
-  const currency = process.env.CURRENCY ?? 'USD'
-  const symbols = process.env.SYMBOLS ?? ''
-  //! TODO: Fetch some other way perhaps
-  const privateKey = process.env.PRIVATE_KEY ?? ''
-
-  const config: PriceSourceConfig = {
-    symbols: symbols.split(',')
-  }
-
-  const priceManager = new PriceManager(config, new IexPriceProvider(apiToken))
-  const prices = await priceManager.fetchAssetPrices()
-
-  const oraclesManager = OraclesManager.withWhaleClient(oceanUrl, network, privateKey)
-  await oraclesManager.updatePrices(oracleId,
-    prices.map(assetPrice => ({
-      token: assetPrice.asset,
-      prices: [{ currency, amount: assetPrice.price }]
-    })))
-
-  console.log(JSON.stringify({ prices, event }))
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ prices, event })
-  }
+  return await handleGenericPriceApiProvider(new IexPriceProvider(apiToken), event)
 };
