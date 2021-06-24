49d963e4cad699bc102eee69d1f1ad74342b58f3
monstrobishi <monstrobishi@gmail.com>
2021-06-25 02:23:15 +0800

    code climate

--- .github/workflows/e2e/mocks.js
+++ .github/workflows/e2e/mocks.js
@@ -1,44 +1,50 @@
 const nock = require('nock')
 
+const finnhubbResponse1 = `{
+  "c": 605,
+  "h": 263.31,
+  "l": 260.68,
+  "o": 261.07,
+  "pc": 259.45,
+  "t": ${Math.floor(Date.now() / 1000)} 
+}`
+
+const finnhubbResponse2 = `{
+  "c": 120,
+  "h": 263.31,
+  "l": 260.68,
+  "o": 261.07,
+  "pc": 259.45,
+  "t": ${Math.floor(Date.now() / 1000)}  
+}`
+
+const finnhubbResponse3 = `{
+  "c": 330,
+  "h": 263.31,
+  "l": 260.68,
+  "o": 261.07,
+  "pc": 259.45,
+  "t": ${Math.floor(Date.now() / 1000)}  
+}`
+
 exports.mockFinnhubbEndpoints = () => {
-nock('https://finnhub.io/api/v1/quote')
-  .get('?symbol=TSLA&token=API_TOKEN')
-  .reply(200, function (_) {
-    return `{
-      "c": 605,
-      "h": 263.31,
-      "l": 260.68,
-      "o": 261.07,
-      "pc": 259.45,
-      "t": ${Math.floor(Date.now() / 1000)} 
-    }`
-  })
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=TSLA&token=API_TOKEN')
+    .reply(200, function (_) {
+      return finnhubbResponse1
+    })
 
-nock('https://finnhub.io/api/v1/quote')
-  .get('?symbol=AAPL&token=API_TOKEN')
-  .reply(200, function (_) {
-    return `{
-      "c": 120,
-      "h": 263.31,
-      "l": 260.68,
-      "o": 261.07,
-      "pc": 259.45,
-      "t": ${Math.floor(Date.now() / 1000)}  
-    }`
-  })
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=AAPL&token=API_TOKEN')
+    .reply(200, function (_) {
+      return finnhubbResponse2
+    })
 
-nock('https://finnhub.io/api/v1/quote')
-  .get('?symbol=FB&token=API_TOKEN')
-  .reply(200, function (_) {
-    return `{
-      "c": 330,
-      "h": 263.31,
-      "l": 260.68,
-      "o": 261.07,
-      "pc": 259.45,
-      "t": ${Math.floor(Date.now() / 1000)}  
-    }`
-  })
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=FB&token=API_TOKEN')
+    .reply(200, function (_) {
+      return finnhubbResponse3
+    })
 }
 
 const tiingoResponse = `[
@@ -102,11 +108,11 @@ const tiingoResponse = `[
 ]`
 
 exports.mockTiingoEndpoints = () => {
-nock('https://api.tiingo.com/iex')
-  .get('/?tickers=TSLA,AAPL,FB&token=API_TOKEN')
-  .reply(200, function (_) {
-    return tiingoResponse
-  })
+  nock('https://api.tiingo.com/iex')
+    .get('/?tickers=TSLA,AAPL,FB&token=API_TOKEN')
+    .reply(200, function (_) {
+      return tiingoResponse
+    })
 }
 
 const iexResponse = `[
@@ -155,9 +161,9 @@ const iexResponse = `[
 ]`
 
 exports.mockIexcloudEndpoints = () => {
-nock('https://cloud.iexapis.com/stable/tops')
-  .get('?symbols=TSLA,AAPL,FB&token=API_TOKEN')
-  .reply(200, function (_) {
-    return iexResponse
-  })
+  nock('https://cloud.iexapis.com/stable/tops')
+    .get('?symbols=TSLA,AAPL,FB&token=API_TOKEN')
+    .reply(200, function (_) {
+      return iexResponse
+    })
 }
