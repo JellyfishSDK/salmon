491f6c93f790e8dc088a5974fcbf42980e620b39
monstrobishi <monstrobishi@gmail.com>
2021-06-25 02:12:53 +0800

    Fix e2e

--- .github/workflows/e2e/test.js
+++ .github/workflows/e2e/test.js
@@ -1,40 +1,35 @@
-const finnhubb = require('../../../dist/finnhubb')
-const tiingo = require('../../../dist/tiingo')
-const iexcloud = require('../../../dist/iexcloud')
 const { JsonRpcClient } = require('@defichain/jellyfish-api-jsonrpc')
 const { GenesisKeys } = require('@defichain/testcontainers')
 const BigNumber = require('bignumber.js')
 const { mockFinnhubbEndpoints, mockTiingoEndpoints, mockIexcloudEndpoints } = require('./mocks')
+const finnhubb = require('../../../dist/finnhubb')
+const tiingo = require('../../../dist/tiingo')
+const iexcloud = require('../../../dist/iexcloud')
 
 const oracleOwner = GenesisKeys[GenesisKeys.length - 1].operator
 const client = new JsonRpcClient('http://playground:playground@localhost:3003')
 
 const waitFor = (condition) => {
   return new Promise((r) => {
-    const testCondition = () => {
-      if (condition()) {
+    const testCondition = async () => {
+      if (await condition()) {
         r()
       } else {
         setTimeout(testCondition, 500)
       }
     }
 
-    setTimeout(testCondition, 500)
+    testCondition()
   })
 }
 
 const waitForTxConfirm = async (txid) => {
   await waitFor(async () => {
-    return (await client.wallet.getTransaction(txid)).confirmations > 0
+    return (await client.wallet.getTransaction(txid)).confirmations > 3
   })
 }
 
-const setupOracle = async (id) => {
-  const oracleList = await client.oracle.listOracles()
-  if (oracleList.length - 1 >= id) {
-    return oracleList[id]
-  }
-
+const setupOracle = async () => {
   const oracleId = await client.oracle.appointOracle(oracleOwner.address, [
     {
       token: 'TSLA',
@@ -61,7 +56,7 @@ beforeAll(async () => {
   process.env.SYMBOLS = 'TSLA,AAPL,FB'
   process.env.PRIVATE_KEY = oracleOwner.privKey
 
-  const txids = await Promise.all(Array(6).fill(client.wallet.sendToAddress(oracleOwner.address, 1)))
+  const txids = await Promise.all(Array(6).fill(0).map(() => client.wallet.sendToAddress(oracleOwner.address, 1)))
   await Promise.all(txids.map(txid => waitForTxConfirm(txid)))
 })
 
@@ -73,28 +68,47 @@ afterAll(async () => {
 
 describe('e2e', () => {
   it('should run all lambda functions', async () => {
-    mockFinnhubbEndpoints()
-    process.env.ORACLE_ID = await setupOracle(0)
-    process.env.API_TOKEN = 'API_TOKEN'
-    await finnhubb.handler({})
-    await waitFor(async () => {
-      return (await client.oracle.getOracleData(process.env.ORACLE_ID)).tokenPrices.length > 0
-    })
+    try {
+      mockFinnhubbEndpoints()
+      const finnhubbOracleId = await setupOracle()
+      process.env.ORACLE_ID = finnhubbOracleId
+      process.env.API_TOKEN = 'API_TOKEN'
+      await finnhubb.handler({})
+      await waitFor(async () => {
+        try {
+          return (await client.oracle.getOracleData(finnhubbOracleId)).tokenPrices.length > 1
+        } catch(e) {
+          return false
+        }
+      })
 
-    mockTiingoEndpoints()
-    process.env.ORACLE_ID = await setupOracle(1)
-    process.env.API_TOKEN = 'API_TOKEN'
-    await tiingo.handler({})
-    await waitFor(async () => {
-      return (await client.oracle.getOracleData(process.env.ORACLE_ID)).tokenPrices.length > 0
-    })
+      mockTiingoEndpoints()
+      const tiingoOracleId = await setupOracle()
+      process.env.ORACLE_ID = tiingoOracleId
+      process.env.API_TOKEN = 'API_TOKEN'
+      await tiingo.handler({})
+      await waitFor(async () => {
+        try {
+          return (await client.oracle.getOracleData(tiingoOracleId)).tokenPrices.length > 0
+        } catch(e) {
+          return false
+        }
+      })
 
-    mockIexcloudEndpoints()
-    process.env.ORACLE_ID = await setupOracle(2)
-    process.env.API_TOKEN = 'API_TOKEN'
-    await iexcloud.handler({})
-    await waitFor(async () => {
-      return (await client.oracle.getOracleData(process.env.ORACLE_ID)).tokenPrices.length > 0
-    })
+      mockIexcloudEndpoints()
+      const iexcloudOracleId = await setupOracle()
+      process.env.ORACLE_ID = iexcloudOracleId
+      process.env.API_TOKEN = 'API_TOKEN'
+      await iexcloud.handler({})
+      await waitFor(async () => {
+        try {
+          return (await client.oracle.getOracleData(iexcloudOracleId)).tokenPrices.length > 0
+        } catch(e) {
+          return false
+        }
+      })
+    } catch(e) {
+      console.log(e)
+    }
   })
 })
