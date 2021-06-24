a0b84d41e701623aa174f5b78548093efe940f5e
monstrobishi <monstrobishi@gmail.com>
2021-06-24 21:38:26 +0800

    Finished e2e

--- .github/workflows/e2e/test.js
+++ .github/workflows/e2e/test.js
@@ -1,69 +1,256 @@
-const finnhubbHandler = require('../../../dist/finnhubb').handler
-const tiingoHandler = require('../../../dist/tiingo').handler
-const iexHandler = require('../../../dist/iexcloud').handler
-const JsonRpcClient = require('@defichain/jellyfish-api-jsonrpc').JsonRpcClient
-const GenesisKeys = require('@defichain/testcontainers').GenesisKeys
+const finnhubb = require('../../../dist/finnhubb')
+const tiingo = require('../../../dist/tiingo')
+const iexcloud = require('../../../dist/iexcloud')
+const { JsonRpcClient } = require('@defichain/jellyfish-api-jsonrpc')
+const { GenesisKeys } = require('@defichain/testcontainers')
+const BigNumber = require('bignumber.js')
+const nock = require('nock')
 
-const fetchOraclesList = async () => {
-  const client = new JsonRpcClient('http://playground:playground@localhost:3003')
-  return await client.oracle.listOracles()
+const oracleOwner = GenesisKeys[GenesisKeys.length - 1].operator
+const client = new JsonRpcClient('http://playground:playground@localhost:3003')
+
+const waitFor = (condition) => {
+  return new Promise((r) => {
+    const testCondition = () => {
+      if(condition()) {
+        r()
+      } else {
+        setTimeout(testCondition, 500)
+      }
+    }
+
+    setTimeout(testCondition, 500)
+  })
+}
+
+const waitForTxConfirm = async (txid) => {
+  await waitFor(async () => {
+    return (await client.wallet.getTransaction(txid)).confirmations > 0
+  })
+}
+
+const setupOracle = async () => {   
+  const oracleId = await client.oracle.appointOracle(oracleOwner.address, [
+    {
+      token: 'TSLA',
+      currency: 'USD'
+    },{
+      token: 'AAPL',
+      currency: 'USD'
+    },{
+      token: 'FB',
+      currency: 'USD'
+    }
+  ], {
+    weightage: 1.0
+  })
+
+  await waitForTxConfirm(oracleId)
+  return oracleId
 }
 
 beforeAll(async () => {
-    process.env.OCEAN_URL = 'http://localhost:3001'
-    process.env.NETWORK = 'regtest'
-    process.env.CURRENCY = 'USD'
-    process.env.SYMBOLS = 'TSLA,AAPL,FB'
-    process.env.PRIVATE_KEY = GenesisKeys[GenesisKeys.length - 1].owner.privKey
+  process.env.OCEAN_URL = 'http://localhost:3001'
+  process.env.NETWORK = 'regtest'
+  process.env.CURRENCY = 'USD'
+  process.env.SYMBOLS = 'TSLA,AAPL,FB'
+  process.env.PRIVATE_KEY = oracleOwner.privKey
+
+  const txids = [
+    await client.wallet.sendToAddress(oracleOwner.address, 1),
+    await client.wallet.sendToAddress(oracleOwner.address, 1),
+    await client.wallet.sendToAddress(oracleOwner.address, 1),
+    await client.wallet.sendToAddress(oracleOwner.address, 1),
+    await client.wallet.sendToAddress(oracleOwner.address, 1),
+    await client.wallet.sendToAddress(oracleOwner.address, 1)
+  ]
+
+  await Promise.all(txids.map(txid => waitForTxConfirm(txid)))
 })
 
 afterAll(async () => {
-  expect(1).toStrictEqual(1)
+  expect(new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'AAPL' }))).toStrictEqual(new BigNumber('130'))
+  expect(new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'FB' }))).toStrictEqual(new BigNumber('340'))
+  expect(new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'TSLA' }))).toStrictEqual(new BigNumber('606'))
 })
 
 describe('e2e', () => {
   it('should run the finnhubb lambda function', async () => {
-    // !TODO: Remove this
-    nock('http://localhost:3001/v0/regtest/address/bcrt1qyeuu9rvq8a67j86pzvh5897afdmdjpyankp4mu/transactions')
-    .get('/unspent?size=10')
-    .reply(200, function (_) {
-      return {
-        data:
-        [{
-          id: '03280abd3d3ae8dc294c1a572cd7912c3c3e53044943eac62c2f6c4687c87f1000000001',
-          hid: 'b115e5ea70e06e0c41a0ba8417ca6c311e29e9f590fc33d61b6b94e623baf207',
-          sort: '0000000003280abd3d3ae8dc294c1a572cd7912c3c3e53044943eac62c2f6c4687c87f1000000001',
-          block: {
-            hash: 'd744db74fb70ed42767ae028a129365fb4d7de54ba1b6575fb047490554f8a7b',
-            height: 0
-          },
-          script: {
-            type: 'witness_v0_keyhash',
-            hex: '00142679c28d803f75e91f41132f4397dd4b76d9049d'
-          },
-          vout: {
-            txid: '03280abd3d3ae8dc294c1a572cd7912c3c3e53044943eac62c2f6c4687c87f10',
-            n: 1,
-            value: '10.00000000'
-          }
-        }]
-      }
-    })
+    nock('https://finnhub.io/api/v1/quote')
+      .get('?symbol=TSLA&token=API_TOKEN')
+      .reply(200, function (_) {
+        return `{
+          "c": 605,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": ${Math.floor(Date.now() / 1000)} 
+        }`
+      })
 
-    process.env.ORACLE_ID = (await fetchOraclesList())[0]
-    process.env.API_TOKEN = 'FINNHUBB_TOKEN'
-    finnhubbHandler({})
+    nock('https://finnhub.io/api/v1/quote')
+      .get('?symbol=AAPL&token=API_TOKEN')
+      .reply(200, function (_) {
+        return `{
+          "c": 120,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": ${Math.floor(Date.now() / 1000)}  
+        }`
+      })
+
+    nock('https://finnhub.io/api/v1/quote')
+      .get('?symbol=FB&token=API_TOKEN')
+      .reply(200, function (_) {
+        return `{
+          "c": 330,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": ${Math.floor(Date.now() / 1000)}  
+        }`
+      })   
+    
+    process.env.ORACLE_ID = await setupOracle()
+    process.env.API_TOKEN = 'API_TOKEN'
+    await finnhubb.handler({})
+    await waitFor(async () => {
+      return (await client.oracle.getOracleData(oracleId)).tokenPrices.length > 0
+    })  
   })
   
   it('should run the tiingo lambda function', async () => {
-    process.env.ORACLE_ID = (await fetchOraclesList())[1]
-    process.env.API_TOKEN = 'TIINGO_TOKEN'
-    tiingoHandler({})
+    nock('https://api.tiingo.com/iex')
+      .get('/?tickers=TSLA,AAPL,FB&token=API_TOKEN')
+      .reply(200, function (_) {
+        return `[
+          {
+            "bidPrice":null,
+            "last":606,
+            "quoteTimestamp":"2021-06-07T20:00:00+00:00",
+            "bidSize":null,
+            "high":610.0,
+            "timestamp":"2021-06-07T20:00:00+00:00",
+            "tngoLast":605.13,
+            "lastSize":null,
+            "askSize":null,
+            "ticker":"TSLA",
+            "askPrice":null,
+            "low":582.88,
+            "open":591.825,
+            "prevClose":599.05,
+            "mid":null,
+            "lastSaleTimestamp": "${(new Date()).toISOString()}",
+            "volume":22543682
+          },
+          {
+            "last":120,
+            "bidPrice":null,
+            "quoteTimestamp":"2021-06-07T20:00:00+00:00",
+            "lastSize":null,
+            "open":126.17,
+            "timestamp":"2021-06-07T20:00:00+00:00",
+            "tngoLast":125.9,
+            "bidSize":null,
+            "askSize":null,
+            "ticker":"AAPL",
+            "askPrice":null,
+            "low":124.8321,
+            "high":126.32,
+            "prevClose":125.89,
+            "mid":null,
+            "lastSaleTimestamp": "${(new Date()).toISOString()}",
+            "volume":71057550
+          },
+          {
+            "last":340,
+            "bidPrice":null,
+            "quoteTimestamp":"2021-06-07T20:00:00+00:00",
+            "lastSize":null,
+            "open":329.48,
+            "timestamp":"2021-06-07T20:00:00+00:00",
+            "tngoLast":336.58,
+            "bidSize":null,
+            "askSize":null,
+            "ticker":"FB",
+            "askPrice":null,
+            "low":328.93,
+            "high":337.69,
+            "prevClose":330.35,
+            "mid":null,
+            "lastSaleTimestamp": "${(new Date()).toISOString()}",
+            "volume":20136707
+          }
+        ]`
+      })
+
+    process.env.ORACLE_ID = await setupOracle()
+    process.env.API_TOKEN = 'API_TOKEN'
+    await tiingo.handler({})
+    await waitFor(async () => {
+      return (await client.oracle.getOracleData(oracleId)).tokenPrices.length > 0
+    })  
   })
 
   it('should run the iexcloud lambda function', async () => {
-    process.env.ORACLE_ID = (await fetchOraclesList())[2]
-    process.env.API_TOKEN = 'IEX_TOKEN'
-    iexHandler({})
+    nock('https://cloud.iexapis.com/stable/tops')
+      .get('?symbols=TSLA,AAPL,FB&token=API_TOKEN')
+      .reply(200, function (_) {
+        return `[
+          {
+            "symbol":"TSLA",
+            "sector":"consumerdurables",
+            "securityType":"cs",
+            "bidPrice":0,
+            "bidSize":0,
+            "askPrice":0,
+            "askSize":0,
+            "lastUpdated":1623096987115,
+            "lastSalePrice":607,
+            "lastSaleSize":10,
+            "lastSaleTime": ${Date.now()},
+            "volume":480662
+          },
+          {
+            "symbol":"AAPL",
+            "sector":"electronictechnology",
+            "securityType":"cs",
+            "bidPrice":0,
+            "bidSize":0,
+            "askPrice":0,
+            "askSize":0,
+            "lastUpdated":1623099600004,
+            "lastSalePrice":150,
+            "lastSaleSize":100,
+            "lastSaleTime": ${Date.now()},
+            "volume":1401779
+          },
+          {
+            "symbol":"FB",
+            "sector":"technologyservices",
+            "securityType":"cs",
+            "bidPrice":0,
+            "bidSize":0,
+            "askPrice":0,
+            "askSize":0,
+            "lastUpdated":1623097044336,
+            "lastSalePrice":350,
+            "lastSaleSize":15,
+            "lastSaleTime": ${Date.now()},
+            "volume":598873
+          }
+        ]`
+      })
+
+    process.env.ORACLE_ID = await setupOracle()
+    process.env.API_TOKEN = 'API_TOKEN'
+    await iexcloud.handler({})
+    await waitFor(async () => {
+      return (await client.oracle.getOracleData(oracleId)).tokenPrices.length > 0
+    })  
   })
 })
