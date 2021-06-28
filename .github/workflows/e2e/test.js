a090421a02f1349602aba274a699a02c01e54ab3
monstrobishi <monstrobishi@gmail.com>
2021-06-28 15:08:47 +0800

    PR suggestions

--- .github/workflows/e2e/test.js
+++ .github/workflows/e2e/test.js
@@ -5,31 +5,12 @@ const { mockFinnhubbEndpoints, mockTiingoEndpoints, mockIexcloudEndpoints } = re
 const finnhubb = require('../../../dist/finnhubb')
 const tiingo = require('../../../dist/tiingo')
 const iexcloud = require('../../../dist/iexcloud')
+const waitForExpect = require('wait-for-expect')
 
 const oracleOwner = GenesisKeys[GenesisKeys.length - 1].operator
 const client = new JsonRpcClient('http://playground:playground@localhost:3003')
 
-const waitFor = (condition) => {
-  return new Promise((r) => {
-    const testCondition = async () => {
-      if (await condition()) {
-        r()
-      } else {
-        setTimeout(testCondition, 500)
-      }
-    }
-
-    testCondition()
-  })
-}
-
-const waitForTxConfirm = async (txid) => {
-  await waitFor(async () => {
-    return (await client.wallet.getTransaction(txid)).confirmations > 3
-  })
-}
-
-const setupOracle = async () => {
+async function setupOracle() {
   const oracleId = await client.oracle.appointOracle(oracleOwner.address, [
     {
       token: 'TSLA',
@@ -45,7 +26,10 @@ const setupOracle = async () => {
     weightage: 1.0
   })
 
-  await waitForTxConfirm(oracleId)
+  await waitForExpect(async () => {
+    expect((await client.wallet.getTransaction(oracleId)).confirmations).toBeGreaterThanOrEqual(3)
+  }, 10000)
+
   return oracleId
 }
 
@@ -56,14 +40,12 @@ beforeAll(async () => {
   process.env.SYMBOLS = 'TSLA,AAPL,FB'
   process.env.PRIVATE_KEY = oracleOwner.privKey
 
-  const txids = await Promise.all(Array(6).fill(0).map(() => client.wallet.sendToAddress(oracleOwner.address, 1)))
-  await Promise.all(txids.map(txid => waitForTxConfirm(txid)))
-})
-
-afterAll(async () => {
-  expect(new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'AAPL' }))).toStrictEqual(new BigNumber('130'))
-  expect(new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'FB' }))).toStrictEqual(new BigNumber('340'))
-  expect(new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'TSLA' }))).toStrictEqual(new BigNumber('606'))
+  for(let i = 0;i < 3;i++) {
+    await waitForExpect(async () => {
+      const txid = await client.wallet.sendToAddress(oracleOwner.address, 1)
+      expect((await client.wallet.getTransaction(txid)).confirmations).toBeGreaterThanOrEqual(3)
+    }, 10000)  
+  }
 })
 
 describe('e2e', () => {
@@ -74,39 +56,39 @@ describe('e2e', () => {
       process.env.ORACLE_ID = finnhubbOracleId
       process.env.API_TOKEN = 'API_TOKEN'
       await finnhubb.handler({})
-      await waitFor(async () => {
-        try {
-          return (await client.oracle.getOracleData(finnhubbOracleId)).tokenPrices.length > 1
-        } catch(e) {
-          return false
-        }
-      })
+
+      await waitForExpect(async () => {
+        expect((await client.oracle.getOracleData(finnhubbOracleId)).tokenPrices.length).toBeGreaterThan(1)
+      }, 10000)
 
       mockTiingoEndpoints()
       const tiingoOracleId = await setupOracle()
       process.env.ORACLE_ID = tiingoOracleId
       process.env.API_TOKEN = 'API_TOKEN'
       await tiingo.handler({})
-      await waitFor(async () => {
-        try {
-          return (await client.oracle.getOracleData(tiingoOracleId)).tokenPrices.length > 0
-        } catch(e) {
-          return false
-        }
-      })
+
+      await waitForExpect(async () => {
+        expect((await client.oracle.getOracleData(tiingoOracleId)).tokenPrices.length).toBeGreaterThan(1)
+      }, 10000)
 
       mockIexcloudEndpoints()
       const iexcloudOracleId = await setupOracle()
       process.env.ORACLE_ID = iexcloudOracleId
       process.env.API_TOKEN = 'API_TOKEN'
       await iexcloud.handler({})
-      await waitFor(async () => {
-        try {
-          return (await client.oracle.getOracleData(iexcloudOracleId)).tokenPrices.length > 0
-        } catch(e) {
-          return false
-        }
-      })
+
+      await waitForExpect(async () => {
+        expect((await client.oracle.getOracleData(iexcloudOracleId)).tokenPrices.length).toBeGreaterThan(1)
+      }, 10000)
+
+      const aaplPrice = new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'AAPL' }))
+      expect(aaplPrice).toStrictEqual(new BigNumber('130'))
+
+      const fbPrice = new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'FB' }))
+      expect(fbPrice).toStrictEqual(new BigNumber('340'))
+
+      const tslaPrice = new BigNumber(await client.oracle.getPrice({ currency: 'USD', token: 'TSLA' }))
+      expect(tslaPrice).toStrictEqual(new BigNumber('606'))    
     } catch(e) {
       console.log(e)
     }
