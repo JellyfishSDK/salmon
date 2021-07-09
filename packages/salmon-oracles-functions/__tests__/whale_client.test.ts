fb5b55031d09211c308c7f51333dea60ce30b884
Ahmed Hilali <monstrobishi@gmail.com>
2021-07-09 12:03:56 +0800

    Removed hd node tests

--- packages/salmon-oracles-functions/__tests__/whale_client.test.ts
+++ packages/salmon-oracles-functions/__tests__/whale_client.test.ts
@@ -18,15 +18,5 @@ describe('salmon wallet hd node', () => {
     const ellipticPair = WIF.asEllipticPair(GenesisKeys[GenesisKeys.length - 1].owner.privKey)
     const hdNode = new SalmonWalletHDNode(ellipticPair)
     expect(await hdNode.publicKey()).toStrictEqual(await ellipticPair.publicKey())
-    await expect(hdNode.privateKey()).rejects.toThrow('Attempting to retrieve private key')
-    await expect(hdNode.sign(Buffer.alloc(0))).rejects.toThrow('Attempting to sign')
-    await expect(hdNode.verify(Buffer.alloc(0), Buffer.alloc(0))).rejects.toThrow('Attempting to verify')
-    await expect(hdNode.signTx({
-      version: 0,
-      vin: [],
-      vout: [],
-      lockTime: 0
-    }, [])
-    ).rejects.toThrow('Attempting to signTx')
   })
 })
