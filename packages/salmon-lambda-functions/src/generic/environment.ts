cce0c1d2779c64243931e4cd2655e45926d18755
Ahmed Hilali <monstrobishi@gmail.com>
2021-07-15 17:03:07 +0800

    Added slack notification (#94)

---
+++ packages/salmon-lambda-functions/src/generic/environment.ts
@@ -0,0 +1,38 @@
+import aws from 'aws-sdk'
+
+export interface EnvironmentConfig {
+  oceanUrl: string
+  slackWebhookUrl: string
+  network: string
+  oracleId: string
+  currency: string
+  symbols: string[]
+  intervalSeconds: number
+  privateKey: string
+}
+
+export const getEnvironmentConfig = async (): Promise<EnvironmentConfig> => {
+  let privateKey = process.env.PRIVATE_KEY ?? ''
+
+  if (process.env.PRIVATE_KEY_SSM_KEY !== undefined) {
+    const paramConfig = {
+      Name: process.env.PRIVATE_KEY_SSM_KEY,
+      WithDecryption: true
+    }
+
+    const parameterStore = new aws.SSM()
+    const param = await parameterStore.getParameter(paramConfig).promise()
+    privateKey = param.Parameter?.Value ?? ''
+  }
+
+  return {
+    oceanUrl: process.env.OCEAN_URL ?? 'localhost',
+    network: process.env.NETWORK ?? 'regtest',
+    oracleId: process.env.ORACLE_ID ?? '',
+    currency: process.env.CURRENCY ?? 'USD',
+    symbols: (process.env.SYMBOLS ?? '').split(','),
+    intervalSeconds: parseInt(process.env.INTERVAL_SECONDS ?? '300'),
+    privateKey,
+    slackWebhookUrl: process.env.SLACK_WEBHOOK_URL ?? ''
+  }
+}
--- packages/salmon-lambda-functions/src/generic/index.ts
+++ packages/salmon-lambda-functions/src/generic/index.ts
@@ -1,44 +1,9 @@
 import { PriceManager, PriceProvider, AssetPrice } from '@defichain/salmon-price-functions'
 import { OraclesManager } from '@defichain/salmon-oracles-functions'
-import aws from 'aws-sdk'
+import { getEnvironmentConfig, EnvironmentConfig } from './environment'
+import { checkBalanceAndNotify } from './slack'
 
-interface EnvironmentConfig {
-  oceanUrl: string
-  network: string
-  oracleId: string
-  currency: string
-  symbols: string[]
-  intervalSeconds: number
-  privateKey: string
-}
-
-const getEnvironmentConfig = async (): Promise<EnvironmentConfig> => {
-  let privateKey = process.env.PRIVATE_KEY ?? ''
-
-  if (process.env.PRIVATE_KEY_SSM_KEY !== undefined) {
-    const paramConfig = {
-      Name: process.env.PRIVATE_KEY_SSM_KEY,
-      WithDecryption: true
-    }
-
-    const parameterStore = new aws.SSM()
-    const param = await parameterStore.getParameter(paramConfig).promise()
-    privateKey = param.Parameter?.Value ?? ''
-  }
-
-  return {
-    oceanUrl: process.env.OCEAN_URL ?? 'localhost',
-    network: process.env.NETWORK ?? 'regtest',
-    oracleId: process.env.ORACLE_ID ?? '',
-    currency: process.env.CURRENCY ?? 'USD',
-    symbols: (process.env.SYMBOLS ?? '').split(','),
-    intervalSeconds: parseInt(process.env.INTERVAL_SECONDS ?? '300'),
-    privateKey
-  }
-}
-
-const broadcastPrices = async (env: EnvironmentConfig, prices: AssetPrice[]): Promise<string | undefined> => {
-  const oraclesManager = OraclesManager.withWhaleClient(env.oceanUrl, env.network, env.privateKey)
+const broadcastPrices = async (oraclesManager: OraclesManager, env: EnvironmentConfig, prices: AssetPrice[]): Promise<string | undefined> => {
   return await oraclesManager.updatePrices(env.oracleId,
     prices.map(assetPrice => ({
       token: assetPrice.asset, prices: [{ currency: env.currency, amount: assetPrice.price }]
@@ -56,9 +21,17 @@ export async function handleGenericPriceApiProvider (provider: PriceProvider, ev
   const prices = await fetchPrices(env, provider)
   console.log(JSON.stringify({ prices, event }))
 
-  const txid = await broadcastPrices(env, prices)
+  const oraclesManager = OraclesManager.withWhaleClient(env.oceanUrl, env.network, env.privateKey)
+  const txid = await broadcastPrices(oraclesManager, env, prices)
   if (txid !== undefined) {
     console.log(`Sent with txid: ${txid}`)
+  }
+
+  // Do this at the end, let critical flow run first
+  try {
+    await checkBalanceAndNotify(oraclesManager, env)
+  } catch (e) {
+    console.log(e)
   }
 
   return {
