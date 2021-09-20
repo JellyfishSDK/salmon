b4e5623720dde666ade3579a53405e82e0e32e99
Ahmed Hilali <monstrobishi@gmail.com>
2021-09-20 11:56:37 +0800

    Begin alpha vantage integration

---
+++ packages/salmon-provider-alpha-vantage/README.md
@@ -0,0 +1,5 @@
+# @defichain/salmon-provider-alpha-vantage
+
+### Config
+
+Requires a Alpha Vantage API token
---
+++ packages/salmon-provider-alpha-vantage/src/index.ts
@@ -0,0 +1,42 @@
+import { PriceProvider, AssetPrice } from '@defichain/salmon-price-functions'
+import fetch from 'node-fetch'
+import BigNumber from 'bignumber.js'
+import {
+  JellyfishJSON
+} from '@defichain/jellyfish-json'
+
+const AV_URL = 'https://www.alphavantage.co/query'
+
+/**
+ * Fetches prices from Alpha Vantage
+ * https://www.alphavantage.co/
+ */
+export class AlphaVantagePriceProvider implements PriceProvider {
+  constructor (
+    private readonly apiToken: string
+  ) {
+  }
+
+  private async fetchAsset (symbol: string): Promise<AssetPrice> {
+    const fetchPath = `${AV_URL}?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${this.apiToken}`
+    const response = await fetch(fetchPath, {
+      method: 'GET'
+    })
+
+    const text = await response.text()
+    const json = JellyfishJSON.parse(text, 'bignumber')
+    const priceObject = json['Global Quote']
+
+    return {
+      asset: symbol,
+      price: new BigNumber(priceObject['05. price']),
+      timestamp: (new BigNumber(json.t)).multipliedBy(1000)
+    }
+  }
+
+  public async prices (symbols: string[]): Promise<AssetPrice[]> {
+    return await Promise.all(symbols.map(async symbol => {
+      return await this.fetchAsset(symbol)
+    }))
+  }
+}
